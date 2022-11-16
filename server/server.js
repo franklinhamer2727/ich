6036cdd999fe652f36fca712a28373cc9caec2f8
franklinhamer2727 <franklinh.jara.o@gmail.com>
2022-11-16 01:39:38 -0500

    nuevos enlaces

---
+++ server/server.js
@@ -0,0 +1,10 @@
+//#configuracion del servidor
+const express= require('express');
+const app = express()
+
+app.get("/api",(req,res) =>{
+    res.json({"users":["UserOne","UserTwo","UserThree","UserFour"]})
+})
+app.listen(5000,()=>{
+    console.log("server started on port 5000");
+})
